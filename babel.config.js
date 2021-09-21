e1b4494055a98cc74a6ebee04c13281f16d2d43f
chisamikan <chisamikan@live.jp>
2021-09-21 22:11:30 +0900

    [add]babel/preset-envの設定追加

--- babel.config.js
+++ babel.config.js
@@ -1,2 +1,10 @@
-const presets = [['@babel/preset-env']];
+const presets = [
+  [
+    '@babel/preset-env',
+    {
+      useBuiltIns: 'entry',
+      corejs: 3,
+    },
+  ],
+];
 module.exports = { presets };
