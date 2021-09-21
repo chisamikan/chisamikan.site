68739050d94dacd44899dfadff9c83a9ba1c7911
chisamikan <chisamikan@live.jp>
2021-09-21 19:15:13 +0900

    [update]細かな調整

--- .imagemin.mjs
+++ .imagemin.mjs
@@ -3,16 +3,20 @@ import imageminMozjpeg from 'imagemin-mozjpeg';
 import imageminPngquant from 'imagemin-pngquant';
 import imageminGifsicle from 'imagemin-gifsicle';
 import imageminSvgo from 'imagemin-svgo';
-import { extendDefaultPlugins } from 'svgo';
 
-const files = await imagemin(['src/images/*.{jpg,png,gif,svg}'], {
+const files = await imagemin(['src/images/**/*.{jpg,png,gif,svg}'], {
   destination: 'dist/images',
   plugins: [
     imageminMozjpeg({ quality: 80 }),
     imageminPngquant({ quality: [0.65, 0.8] }),
     imageminGifsicle(),
     imageminSvgo({
-      plugins: extendDefaultPlugins([{ name: 'removeViewBox', active: false }]),
+      plugins: [
+        {
+          name: 'removeViewBox',
+          active: false,
+        },
+      ],
     }),
   ],
 });
