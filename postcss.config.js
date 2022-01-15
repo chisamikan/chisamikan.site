46122d3d50cc9f0761be373846d891bb6a8326d9
chisamikan <chisamikan@live.jp>
2022-01-16 01:20:58 +0900

    [update]各種パッケージのアップデート

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,21 @@
-const cssnano = require('cssnano');
-
 module.exports = {
   plugins: [
-    require('postcss-import')({
-      plugins: [require('stylelint')],
-    }),
     require('tailwindcss'),
     require('postcss-nested'),
     require('postcss-nested-ancestors'),
-    require('autoprefixer'),
     require('postcss-sort-media-queries')({
-      sort: function (a, b) {},
+      sort: 'mobile-first',
     }),
-    cssnano({ preset: 'default' }),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-aspect-ratio-polyfill'),
+    require('autoprefixer'),
+    require('cssnano')({
+      preset: [
+        'default',
+        {
+          calc: false,
+        },
+      ],
+    }),
   ],
 };
