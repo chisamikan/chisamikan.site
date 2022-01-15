2ad8d86c148c8f6a10d13f4ff9ab5d99b32c1409
chisamikan <chisamikan@live.jp>
2022-01-16 03:24:49 +0900

    [update]JITモードに合わせたCSSコーディングの改良

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   content: ['./src/**/*.{js,ejs}'],
   theme: {
+    fontFamily: {
+      body: ['"M PLUS 1p"', 'sans-serif'],
+      mono: ['"Share Tech Mono"', '"M PLUS 1p"', 'monospace'],
+    },
     extend: {},
   },
   variants: {},
