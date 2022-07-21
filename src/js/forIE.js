6671104fcc644483352a4dd4c352049d3e534695
chisamikan <chisamikan@live.jp>
2022-07-21 23:12:46 +0900

    [update]コードの整理

--- src/js/forIE.js
+++ src/js/forIE.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
   const userAgent = window.navigator.userAgent.toLocaleLowerCase();
   if (userAgent.indexOf('msie') != -1 || userAgent.indexOf('trident') != -1) {
-    const forIE = document.getElementById('forIE');
-    forIE.style.display = 'flex';
+    const forie = document.getElementById('forie');
+    forie.style.display = 'flex';
     console.log(userAgent);
 
     // for win10
     if (userAgent.indexOf('windows nt 10') != -1) {
-      const forWin10 = document.getElementById('forWin10');
-      forWin10.style.display = 'inline-block';
+      const forwin10 = document.getElementById('forwin10');
+      forwin10.style.display = 'inline-block';
     }
   }
 });
