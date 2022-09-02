236b8c886d73ec2a2b409cc3e5585ce5ffe9a971
chisamikan <chisamikan@live.jp>
2022-09-02 23:29:05 +0900

    [remove]使用パッケージの整理

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,6 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss/nesting'),
-    require('postcss-nested-ancestors'),
     require('tailwindcss'),
     require('postcss-sort-media-queries')({
       sort: 'mobile-first',
@@ -10,13 +9,5 @@ module.exports = {
     require('postcss-flexbugs-fixes'),
     require('postcss-aspect-ratio-polyfill'),
     require('autoprefixer'),
-    require('cssnano')({
-      preset: [
-        'default',
-        {
-          calc: false,
-        },
-      ],
-    }),
   ],
 };
