d5040d858a3f6e2c8173791c21b9975d0edb62a3
chisamikan <chisamikan@live.jp>
2022-09-25 17:19:53 +0900

    [update]#30 RSSの取得先変更
    Herokuの終了対策

--- rss-parser.js
+++ rss-parser.js
@@ -3,9 +3,7 @@ const fs = require('fs');
 let parser = new Parser();
 
 (async () => {
-  let feed = await parser.parseURL(
-    'https://pixivfanboxrss.herokuapp.com/fanbox/chisamikan',
-  );
+  let feed = await parser.parseURL('https://rsshub.app/fanbox/chisamikan');
 
   feed.items.length = 5; //一度に表示する件数
   (feed.siteName = 'chisamikan.site'),
