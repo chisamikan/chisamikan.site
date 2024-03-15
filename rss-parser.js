f14a314c1125c049c560569981198789d8a1d613
chisamikan <chisamikan@live.jp>
2024-03-15 23:02:07 +0900

    [fix]rsshubのfanboxがまた動かなくなったので差し替え

--- rss-parser.js
+++ rss-parser.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 let parser = new Parser();
 
 (async () => {
-  let feed = await parser.parseURL('https://rsshub.rss.tips/fanbox/chisamikan');
+  let feed = await parser.parseURL(
+    'https://rsshub-instance.zeabur.app/fanbox/chisamikan',
+  );
 
   feed.items.length = 5; //一度に表示する件数
   (feed.siteName = 'chisamikan.site'),
