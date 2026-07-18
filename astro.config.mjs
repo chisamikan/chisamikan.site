05ffd21b878a40f94ec386723454ba55894ae982
chisamikan <chisamikan@live.jp>
2026-07-19 00:26:45 +0900

    [add]#96 OGP設定の追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import cloudflare from '@astrojs/cloudflare';
 // 各ページ側で `export const prerender = false` を指定することで
 // Cloudflare Pages Functions 上のサーバー処理として動かします。
 export default defineConfig({
-  site: 'https://example.pages.dev', // 実際のドメインに変更してください
+  site: 'https://chisamikan.site',
   output: 'hybrid', // 既定は静的生成(SSG)。/api/contact のみ prerender=false でサーバー実行
   adapter: cloudflare({
     imageService: 'compile',
