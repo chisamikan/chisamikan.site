c982405b72a4ca6c987017d74912c9c6872af77b
chisamikan <chisamikan@live.jp>
2026-07-18 15:53:10 +0900

    [add]#78 SNSリンクを追加

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -12,3 +12,13 @@ export const nav = [
   { href: '/works', label: 'Works' },
   { href: '/#contact', label: 'Contact' },
 ];
+
+export const snsLinks = [
+  { label: 'X', href: 'https://x.com/chisamikan', icon: 'fa-brands fa-x-twitter' },
+  // Misskeyの公式アイコンはFont Awesomeに存在しないため、カスタムSVG(MisskeyIcon)を使用する
+  { label: 'Misskey.io', href: 'https://misskey.io/@chisamikan', icon: 'custom:misskey' },
+  { label: 'pixiv', href: 'https://www.pixiv.net/users/263417', icon: 'fa-brands fa-pixiv' },
+  { label: 'YouTube', href: 'https://www.youtube.com/user/chisamikan', icon: 'fa-brands fa-youtube' },
+  { label: 'Discord', href: 'https://discord.com/invite/KhYNuGStvj', icon: 'fa-brands fa-discord' },
+  { label: 'GitHub', href: 'https://github.com/chisamikan', icon: 'fa-brands fa-github' },
+];
